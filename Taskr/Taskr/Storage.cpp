76c4da4e1e23ae23ec7da2966bc29ada67157651
Qinfei <helenhan41@hotmail.com>
2015-03-23 15:42:14 +0800

    Storage readFile() changes

--- Taskr/Taskr/Storage.cpp
+++ Taskr/Taskr/Storage.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 Storage::Storage() {
 	_filename = "Taskr.txt";
+	readFile();
 }
 
 Storage::~Storage() {
@@ -14,60 +15,46 @@ Storage::~Storage() {
 //NOTE: this will only work for our V0.1 implementation. will need to update this when Task has more attributes, probably with
 //a getline function and then parse the Task accordingly. I just realized this is what you wanted to do with your readFile() method, so sorry about that..
 vector<Task> Storage::getAllTasks() {
-	vector<Task> tempVector;
-	ifstream inFile(_filename);
-	while (true) {
-		std::string taskContent;
-		std::string thrash;
-		int index;
-		Task tempTask;
-
-		inFile >> index >> thrash;
-		getline(inFile, taskContent);
-		tempTask.setDescription(taskContent);
-		tempVector.push_back(tempTask);
-		if (inFile.eof()) {
-			break;
-		}
-	}
-	inFile.close();
-	return tempVector;
+	return _listOfTasks;
 }
 
-//void Storage::readFile() {
-//	ifstream myFile(_filename, ifstream::in);
-//	_listOfTasks.clear();
-//
-//	while (!myFile.eof()) {
-//		Task task;
-//		string description;
-//		int index;
-//		bool isDone;
-//		getline(myFile, description);
-//		myFile >> index >> isDone;
-//
-//		task.setDescription(description);
-//		if (isDone) {
-//			task.setAsDone();
-//		}
-//		_listOfTasks.push_back(task);
-//	}
-//	return;
-//}
+void Storage::readFile() {
+  ifstream file(_filename, ifstream::in);
+  _listOfTasks.clear();
+  
+  int index = 0;
+  while (!file.eof()) {
+    Task task;
+    string description;
+    string emptyLine;
+    bool isDone;
+
+    if (!(file >> isDone)) {
+    	// eof
+    	break;
+    }
+    getline(file, emptyLine);
+    getline(file, description);
+    task.setDescription(description);
+	if (isDone) {
+		task.setAsDone();
+    }
+    _listOfTasks.push_back(task);
+  }
+  return;
+}
 
 void Storage::saveFile(vector<Task> listOfTasks) {
 	ofstream outFile(_filename, ofstream::out);
 	for (int i = 0; i < listOfTasks.size(); i++) {
-		if (!listOfTasks[i].isDone()) {
-			outFile << i + 1 << ". " << listOfTasks[i].getDescription() << endl;
-		}
+		outFile << listOfTasks[i].isDone() << endl;
+		outFile << listOfTasks[i].getDescription() << endl;
 	}
 	outFile.close();
 }
 
 void Storage::addTask(int index, Task task) {
-	fstream outFile;
-	outFile.open(_filename, fstream::out | fstream::app);
-	outFile << index << ". " << task.toString() << endl;
-	outFile.close();
+	_listOfTasks.push_back(task);
+	saveFile(_listOfTasks);
 }
+
