ba3b28c8ca69bcd0d925e9c246911a8d8c11761f
Qinfeiii <helenhan41@hotmail.com>
2015-04-09 10:48:15 +0800

    Task unit testing
    In progress

---
+++ Taskr/TaskUnitTest/TaskUnitTest.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TaskUnitTest
+{
+	TEST_CLASS(TaskUnitTest)
+	{
+	public:
+
+		TEST_METHOD(setDescription_TEST)
+		{
+			//test partition of string consisting of numbers and letters
+			Task task;
+			task.setDescription("finish cs2103");
+			std::string description = "finish cs2103";
+			Assert::AreEqual(task.getDescription(), description);
+
+			//test partition of string consisting of only numbers
+			task.setDescription("123");
+			description = "123";
+			Assert::AreEqual(task.getDescription(), description);
+
+		}
+
+		
+
+	};
+}
+
